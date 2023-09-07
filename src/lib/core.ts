b1d40d658a84f683408e14438fb6e4543df2e8fc
Lautaro Gruss <lautapercuspain@gmail.com>
2023-09-07 14:49:27 -0700

    refactor

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,5 +1,5 @@
 import { generateSuggestion, getChangedFiles } from './utils'
-import axios from 'axios'
+import fetch from 'node-fetch'
 // import { Octokit } from '@octokit/core'
 // import { createAppAuth } from '@octokit/auth-app'
 const messageForNewPRs = "We're analyzing the contents of the PR's files in order to create unit tests for it."
@@ -77,7 +77,7 @@ export async function handlePullRequestOpened({ payload, octokit }) {
 
   return getChangedFiles({ owner, repo, pullRequestNumber, octokit }).then(async (changedFiles) => {
     // console.log('Changed files:')
-    changedFiles.forEach((file) => {
+    changedFiles.forEach(async (file) => {
       console.info('File:', file.filename)
       console.log('Status:', file.status)
       console.log('Additions:', file.additions)
@@ -90,72 +90,60 @@ export async function handlePullRequestOpened({ payload, octokit }) {
       let relativePath = file.filename.split('/').slice(0, -1).join('/')
       let lastPart = file.filename.split('/').pop()
       let [filename, extension] = lastPart.split('.')
+      const response = await fetch(rawUrl)
+      const fileContents = await response.text()
+      console.log('fileContents:', fileContents)
 
-      axios
-        .get(rawUrl)
-        .then(async (response) => {
-          console.log('axios response:')
-          if (response.status === 200) {
-            const fileContents = response.data
-            console.log(fileContents)
+      suggestions = await generateSuggestion(fileContents, depList)
+      console.log('suggestions', suggestions)
+      prediction = suggestions[0].message.content
+      console.log('Prediction: ', prediction)
 
-            suggestions = await generateSuggestion(fileContents, depList)
-            console.log('suggestions', suggestions)
-            prediction = suggestions[0].message.content
-            console.log('Prediction: ', prediction)
-            if (prediction.length > 0) {
-              try {
-                await octokit.request(`POST /repos/${owner}/${repo}/issues/${pullRequestNumber}/comments`, {
-                  body: `A test has been generated for the filename: ${file.filename}`,
-                  headers: {
-                    'x-github-api-version': '2022-11-28',
-                    Accept: 'application/vnd.github+json',
-                  },
-                })
-              } catch (error) {
-                if (error.response) {
-                  console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
-                }
-                console.error(error)
-              }
-              ;(async () => {
-                // console.log('lastPart:', lastPart)
-                // console.log('extension:', extension)
-                // console.log('filename:', filename)
-                try {
-                  //Ensure aren't creating a test for a test itself.
-                  if (extension !== 'test') {
-                    await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
-                      owner,
-                      repo,
-                      path: `${relativePath}/${filename.toLowerCase()}.test.${extension}`,
-                      branch: payload.pull_request.head.ref,
-                      message: `new test added for the ${filename} component.`,
-                      committer: {
-                        name: 'Code Genius',
-                        email: 'geniuscodeai@gmail.com',
-                      },
-                      content: btoa(
-                        prediction.replace('```', '').replace('javascript', '').replace('jsx', '').replace('```', '')
-                      ),
-                      headers: {
-                        'X-GitHub-Api-Version': '2022-11-28',
-                      },
-                    })
-                    // console.log('PR updated successfully:', response.data)
-                  }
-                } catch (error) {
-                  console.error('Error updating PR:', error)
-                }
-              })()
-            }
-          } else {
-            console.log(`Failed to retrieve file contents. Status code: ${response.status}`)
+      if (prediction.length > 0) {
+        try {
+          await octokit.request(`POST /repos/${owner}/${repo}/issues/${pullRequestNumber}/comments`, {
+            body: `A test has been generated for the filename: ${file.filename}`,
+            headers: {
+              'x-github-api-version': '2022-11-28',
+              Accept: 'application/vnd.github+json',
+            },
+          })
+        } catch (error) {
+          if (error.response) {
+            console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
           }
-        })
-        .catch((error) => {
-          console.error('An error occurred:', error)
-        })
+          console.error(error)
+        }
+        ;(async () => {
+          // console.log('lastPart:', lastPart)
+          // console.log('extension:', extension)
+          // console.log('filename:', filename)
+          try {
+            //Ensure we aren't creating a test for a test itself.
+            const path = `${relativePath}/${filename.toLowerCase()}.test.${extension}`
+            if (extension !== 'test') {
+              await octokit.request(`PUT /repos/${owner}/${repo}/contents/${path}`, {
+                branch: payload.pull_request.head.ref,
+                message: `A new test was added to cover ${filename}.`,
+                committer: {
+                  name: 'Lautaro Gruss',
+                  email: 'lautapercuspain@gmail.com',
+                },
+                content: btoa(
+                  prediction.replace('```', '').replace('javascript', '').replace('jsx', '').replace('```', '')
+                ),
+                headers: {
+                  'X-GitHub-Api-Version': '2022-11-28',
+                  Accept: 'application/vnd.github+json',
+                },
+              })
+              // console.log('PR updated successfully:', response.data)
+            }
+          } catch (error) {
+            console.error('Error updating PR:', error)
+          }
+        })()
+      }
     })
   })
 }
