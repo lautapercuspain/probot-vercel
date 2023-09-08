06075d79730903e879f9e847e6871cfe62771fae
Lautaro Gruss <lautapercuspain@gmail.com>
2023-09-07 17:24:11 -0700

    runtime

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,9 @@
 import { createNodeMiddleware, createProbot } from 'probot'
 import app from './app'
 
-export const config = {
-  runtime: 'edge',
-}
+// export const config = {
+//   runtime: 'edge',
+// }
 
 const probot = createProbot()
 
