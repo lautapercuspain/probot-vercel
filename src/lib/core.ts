fb252de72048421709ed768f924089c758babffd
Lautaro Gruss <lautapercuspain@gmail.com>
2023-09-09 02:20:45 -0700

    a onda

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -17,18 +17,19 @@ export async function handlePullRequestOpened({ payload, octokit, openai }) {
   const owner = payload.repository.owner.login
   const repo = payload.repository.name
   const pullRequestNumber = payload.pull_request.number
-  await octokit.request(`POST /repos/${owner}/${repo}/issues/${pullRequestNumber}/comments`, {
-    body: messageForNewPRs,
-    headers: {
-      'x-github-api-version': '2022-11-28',
-      Accept: 'application/vnd.github+json',
-      // Authorization: `Bearer ${appAuthentication.token}`,
-    },
-  })
-
+  await octokit
+    .request(`POST /repos/${owner}/${repo}/issues/${pullRequestNumber}/comments`, {
+      body: messageForNewPRs,
+      headers: {
+        'x-github-api-version': '2022-11-28',
+        Accept: 'application/vnd.github+json',
+        // Authorization: `Bearer ${appAuthentication.token}`,
+      },
+    })
+    .then((data) => console.log(data))
   // console.log('res:', res)
 
-  // console.log(`Branch Name:`, payload.pull_request.head.ref)
+  console.log(`Branch Name:`, payload.pull_request.head.ref)
   // console.dir(payload);
 
   //Get the contents of package json.
@@ -54,7 +55,7 @@ export async function handlePullRequestOpened({ payload, octokit, openai }) {
   await getChangedFiles({ owner, repo, pullRequestNumber, octokit }).then(async (changedFiles) => {
     changedFiles.forEach(async (file) => {
       rawUrl = file.blobUrl.replace('/blob/', '/raw/')
-      // console.log('rawUrl:', rawUrl)
+      console.log('rawUrl:', rawUrl)
 
       relativePath = file.filename.split('/').slice(0, -1).join('/')
       const lastPart = file.filename.split('/').pop()
@@ -70,9 +71,45 @@ export async function handlePullRequestOpened({ payload, octokit, openai }) {
 
     fileContents = await fileRes.text()
 
-    // console.log('File contents:', fileContents)
+    console.log('File contents:', fileContents)
 
-    const payloadOpenAI = getOpenAIPayload(depList, fileContents)
+    const payloadOpenAI: OpenAI.Chat.ChatCompletionCreateParams = {
+      messages: [
+        {
+          role: 'system',
+          content: `You are an expert software agent in unit test.
+              Please follow these guilines:
+              - Always pass all the props that the component in expecting in all tests.
+              - Consistently presume that the component to be tested resides within the identical directory as the generated test.
+              - Don't use getByTestId if the passed component don't support it.
+              - Always use the waitFor method from the testing-library/react package, and don't forget to import that in the testing code.
+              - Try to use the getByRole method, if target element is a generic role, other wise use the getByText method to find elements when you see fit.
+                Examples: Given the following html: <a href="/about">About</a>, if could do this inside a test:
+                import {render, screen} from '@testing-library/react'
+                render(<MyComponent />)
+                const aboutAnchorNode = screen.getByText(/about/i, {exact: false}). // Use the i or the second argument with exact equal to false to make it more flexible to find the element.
+                // Or use a custom function: screen.getByText((content, element) => content.startsWith('Hello'))
+              - Make sure to avoid this error in tests: Matcher error: received value must be a mock or spy function.
+                Use mock functions in tests, for example, use the jest.spyOn to create a mock for clearInterval on the window object.
+                For the case of clearInterval, it would be something like this: const clearIntervalMock = jest.spyOn(window, 'clearInterval');
+              - Always use fireEvent instead of userEvent i.e: fireEvent.change(input, { target: { value: newValue }});
+              - Avoid using the rerender method to test that some state changes, intead use the mentioned fireEvent method to do that.
+              - It's important to ensure that each test case starts with a clean and isolated state.
+              - Ensure that each test is isolated from the others. One test's behavior shouldn't affect another test's outcome.
+              - If you use the act method, don't forget to import it from the react testing library lib.
+              - To implment clean up after each test you could use, cleanup from react testing library in conjuntion with the afterEach method.
+              `,
+        },
+        {
+          role: 'user',
+          content: `
+              Create at least three unit tests, using the following libs: ${depList}, for the following code: ${fileContents}.
+              But don't add explanations or triple backtick to the output.`,
+        },
+      ],
+      model: 'gpt-4',
+      temperature: 0.9,
+    }
 
     // console.log('payloadOpenAI:', payloadOpenAI)
 
@@ -81,20 +118,20 @@ export async function handlePullRequestOpened({ payload, octokit, openai }) {
     console.log('completion:', completion)
 
     if (completion) {
-      // try {
-      //   await octokit.request(`POST /repos/${owner}/${repo}/issues/${pullRequestNumber}/comments`, {
-      //     body: `A test has been generated for the filename: ${filename}`,
-      //     headers: {
-      //       'x-github-api-version': '2022-11-28',
-      //       Accept: 'application/vnd.github+json',
-      //     },
-      //   })
-      // } catch (error) {
-      //   if (error.response) {
-      //     console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
-      //   }
-      //   console.error(error)
-      // }
+      try {
+        await octokit.request(`POST /repos/${owner}/${repo}/issues/${pullRequestNumber}/comments`, {
+          body: `A test has been generated for the filename: ${filename}`,
+          headers: {
+            'x-github-api-version': '2022-11-28',
+            Accept: 'application/vnd.github+json',
+          },
+        })
+      } catch (error) {
+        if (error.response) {
+          console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
+        }
+        console.error(error)
+      }
 
       // console.log('lastPart:', lastPart)
       // console.log('extension:', extension)
