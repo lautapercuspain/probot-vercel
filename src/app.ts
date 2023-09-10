f5ac4436ad2bff908a590e3167f9187aa28ddb50
Lautaro Gruss <lautapercuspain@gmail.com>
2023-09-10 13:23:45 -0700

    comment out

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ import { handlePullRequestOpened } from './lib/core'
 export default (app: Probot) => {
   //Listen to pull requests events
   app.on(['pull_request.opened'], async (context: Context) => {
-    console.log('context.payload,', context.payload)
+    // console.log('context.payload,', context.payload)
 
     return handlePullRequestOpened({
       context,
